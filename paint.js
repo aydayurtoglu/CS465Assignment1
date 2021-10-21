cbcfe024ff2a4c205d105b9258cbc8f174f82334
DESKTOP-HROFD7S\bupsd <bupsdogatansel@gmail.com>
2021-10-21 21:53:27 +0300

    .

--- paint.js
+++ paint.js
@@ -14,13 +14,13 @@ var points =  [];
 
 var colors = [
 
-  vec4(0.0, 0.0, 0.0, 1.0), // black
-  vec4(1.0, 0.0, 0.0, 1.0), // red
-  vec4(1.0, 1.0, 0.0, 1.0), // yellow
-  vec4(0.0, 1.0, 0.0, 1.0), // green
-  vec4(0.0, 0.0, 1.0, 1.0), // blue
-  vec4(1.0, 0.0, 1.0, 1.0), // magenta
-  vec4(0.0, 1.0, 1.0, 1.0) // cyan
+    vec4(0.0, 0.0, 0.0, 1.0), // black
+    vec4(1.0, 0.0, 0.0, 1.0), // red
+    vec4(1.0, 1.0, 0.0, 1.0), // yellow
+    vec4(0.0, 1.0, 0.0, 1.0), // green
+    vec4(0.0, 0.0, 1.0, 1.0), // blue
+    vec4(1.0, 0.0, 1.0, 1.0), // magenta
+    vec4(0.0, 1.0, 1.0, 1.0) // cyan
 ];
 var t;
 var numPolygons = 0;
@@ -28,23 +28,25 @@ var numIndices = [];
 numIndices[0] = 0;
 var start = [0];
 var undoOperations=[10];
+var redoOperations=[10];
 var undoNo = 0;
+var redoNo = 0;
 
 var mouseClicked = false;
 
 window.onload = function init() {
-  canvas = document.getElementById("gl-canvas");
+    canvas = document.getElementById("gl-canvas");
 
-  gl = WebGLUtils.setupWebGL(canvas);
-  if (!gl) {
-    alert("WebGL isn't available");
-  }
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        alert("WebGL isn't available");
+    }
 
-  var m = document.getElementById("mymenu");
+    var m = document.getElementById("mymenu");
 
-  m.addEventListener("click", function() {
-    cindex = m.selectedIndex;
-  });
+    m.addEventListener("click", function() {
+        cindex = m.selectedIndex;
+    });
 
     var c = document.getElementById("clearButton")
     c.addEventListener("click", function(){
@@ -58,106 +60,128 @@ window.onload = function init() {
     var undo = document.getElementById("undoButton")
     undo.addEventListener("click", function(){
         
-        if (undoNo == 9)
+        if (undoNo == 10 || (undoNo == 0 && redoNo != 0))
             return;
 
-        undoOperations[undoNo] = numIndices[numPolygons];
-        numIndices[numPolygons] = 0;
-        numPolygons = numPolygons - 1;
-        undoNo++;
+        else {
+            undoOperations[undoNo] = numIndices[numPolygons];
+            numIndices[numPolygons] = 0;
+            numPolygons--;
+            undoNo++;
+        }
+        
 
-  });
+    });
 
-  var redo = document.getElementById("clearButton")
-  redo.addEventListener("click", function(){
+    var redo = document.getElementById("clearButton")
+    redo.addEventListener("click", function redo(){
 
-    if (undoNo == 0)
+        //redo a Stroke by pulling data from redoOperations array
+        if (redoNo != 0 && undoNo == 0 ){
+            numPolygons++;
+            numIndices[numPolygons] = redoOperations[redoNo];
+            redoOperations[redoNo] = 0;
+            redoNo--;
+        }
+        //redo a stroke by pulling data from undoOperations array
+        else if (undoNo != 0) {
+            numPolygons++;
+            numIndices[numPolygons] = undoOperations[undoNo];
+            undoOperations[undoNo] = 0;
+            undoNo--;
+        }
+        else {
             return;
+        }
+        
+        
+    });
+  
+    canvas.addEventListener("mousedown", function draw(event){
+        mouseClicked = true;
+
+        for (var i = undoNo; i > 0; i-- ){
+            redoOperations[i] = undoOperations[i];
+            undoOperations[i] = 0;
+        }
+        redoNo = undoNo;
+        undoNo = 0;
 
         numPolygons++;
-        numIndices[numPolygons] = undoOperations[undoNo];
-        undoOperations[undoNo] = 0;
-        undoNo--;
-        
-  });
+        numIndices[numPolygons] = 0;
+        start[numPolygons] = index;
+    });
   
-  canvas.addEventListener("mousedown", function(event){
-    mouseClicked = true;
-    numPolygons++;
-    numIndices[numPolygons] = 0;
-    start[numPolygons] = index;
-  });
+    canvas.addEventListener("mouseup", function release(event){
+        mouseClicked = false;
+    });
   
-  canvas.addEventListener("mouseup", function(event){
-    mouseClicked = false;
-  });
-  
-  canvas.addEventListener("mousemove", function(event) {
-    if(mouseClicked){
-        var r = 2000;
-        t = vec2(2 * event.clientX / canvas.width - 1,
-        2 * (canvas.height - event.clientY) / canvas.height - 1);
-        gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-        gl.bufferSubData(gl.ARRAY_BUFFER, 8 * index, flatten(t));
+    canvas.addEventListener("mousemove", function stroke(event) {
+        if(mouseClicked){
+            var r = 2000;
+            t = vec2(2 * event.clientX / canvas.width - 1,
+                2 * (canvas.height - event.clientY) / canvas.height - 1);
+            gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+            gl.bufferSubData(gl.ARRAY_BUFFER, 8 * index, flatten(t));
 
-        t = vec4(colors[cindex]);
+            t = vec4(colors[cindex]);
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, cBufferId);
-        gl.bufferSubData(gl.ARRAY_BUFFER, 16 * index, flatten(t));
+            gl.bindBuffer(gl.ARRAY_BUFFER, cBufferId);
+            gl.bufferSubData(gl.ARRAY_BUFFER, 16 * index, flatten(t));
 
-        numIndices[numPolygons]++;
-        index++;
+            numIndices[numPolygons]++;
+            index++;
 
-        var center = vec2(event.clientX, event.clientY); 
-   
-        points.push(center);
-        for (var i = 0; i <= 100; i++){
-            points.push(center + vec2(
-                r*Math.cos(i * 2 * Math.PI / 200),
-                r*Math.sin(i * 2 * Math.PI / 200) 
-            ));
+            var center = vec2(event.clientX, event.clientY); 
+    
+            points.push(center);
+            for (var i = 0; i <= 100; i++){
+                points.push(center + vec2(
+                    r*Math.cos(i * 2 * Math.PI / 200),
+                    r*Math.sin(i * 2 * Math.PI / 200) 
+                ));
+            }
         }
-    }
-  });
+    });
 
-  gl.viewport(0, 0, canvas.width, canvas.height);
-  gl.clearColor(0.8, 0.8, 0.8, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.8, 0.8, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-  //
-  //  Load shaders and initialize attribute buffers
-  //
-  var program = initShaders(gl, "vertex-shader", "fragment-shader");
-  gl.useProgram(program);
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
 
-  var bufferId = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-  gl.bufferData(gl.ARRAY_BUFFER, 8 * maxNumVertices, gl.STATIC_DRAW);
-  var vPos = gl.getAttribLocation(program, "vPosition");
-  gl.vertexAttribPointer(vPos, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vPos);
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, 8 * maxNumVertices, gl.STATIC_DRAW);
+    var vPos = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPos, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPos);
 
-  var cBufferId = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, cBufferId);
-  gl.bufferData(gl.ARRAY_BUFFER, 16 * maxNumVertices, gl.STATIC_DRAW);
-  var vColor = gl.getAttribLocation(program, "vColor");
-  gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(vColor);
+    var cBufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, 16 * maxNumVertices, gl.STATIC_DRAW);
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
 
-  render();
+    render();
 }
 
 function render() {
 
-  gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-  for (var i = 0; i <= numPolygons; i++) {
-    gl.drawArrays(gl.LINE_STRIP, start[i], numIndices[i]);
-  }
+    for (var i = 0; i <= numPolygons; i++) {
+        gl.drawArrays(gl.LINE_STRIP, start[i], numIndices[i]);
+    }
 
-  setTimeout(
-    function() {
-      requestAnimFrame(render);
-    }, delay
-  );
+    setTimeout(
+        function() {
+            requestAnimFrame(render);
+        }, delay
+    );
 }
